fb732d1cd35807f49ed65b78a47edcc939437c9d
davetechtreck <techtr3ck@gmail.com>
2026-09-08 13:19:11 -0400

    Update build.py

--- build.py
+++ build.py
@@ -36,8 +36,6 @@ POSTS_DIR = os.path.join(ROOT, "posts")
 DIST_DIR = os.path.join(ROOT, "dist")
 
 
-
-<meta name="google-site-verification" content="rqQ61LFUiuVpSxe5tE5XCKxoUCfLE-sS56rFkxEk-Ks" />
 # ---------------------------------------------------------------------------
 # Frontmatter parsing
 # ---------------------------------------------------------------------------
